42a0a1564ff391baab7f9e65adfdfcb7978ce1b8
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-11 15:42:27 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>patie 9 exercice 4</title>
+</head>
+<body>
+    <?php 
+        $date = mktime(15, 0, 0, 8, 2, 2016);
+    ?>
+    <?= 'Timestamp 2 aout 2016 15h00 : ' .$date  ?>
+</body>
+</html>
+
+
