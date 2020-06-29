dd94360b97a00b7fcfc14d4f69ea0dd87983910d
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-29 14:15:19 +0200

    corection

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+        //retourne le timestamp du 2/08/2016 a 15h00
+        $date = mktime(15, 0, 0, 8, 2, 2016);
+        // on declare les objet
+        $dateOBj = new DateTime();
+        $augustDate = new DateTime('2016-08-02 15:00');
+    ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
@@ -6,10 +13,9 @@
     <title>patie 9 exercice 4</title>
 </head>
 <body>
-    <?php 
-        $date = mktime(15, 0, 0, 8, 2, 2016);
-    ?>
-    <p><?= 'Timestamp actuel : ' . time() ?></p>
+    <p><?= 'Timestamp actuel : ' . time()  //affiche le timestamps a l'instant T?></p>
     <p><?= 'Timestamp 2 aout 2016 15h00 : ' .$date  ?></p>
+    <p><?= 'Actuel Format objet : ' . $dateOBj -> getTimestamp(); ?></p>
+    <p><?= '2 Aout : ' . $augustDate -> getTimestamp() ?></p>
 </body>
 </html>
